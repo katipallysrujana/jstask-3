238393cb66e85c0db0d199660a966feb573fa2d9
unknown <katipallysrujana@gmail.com>
2024-10-15 11:48:41 +0530

    arrays

---
+++ task.js
@@ -0,0 +1,63 @@
+// var a= [2,4,6,8,10,12,14,16,18,20]
+// console.log(a.length)
+// a.push(22);
+// console.log(a)
+// a.pop();
+// console.log(a)
+// a.unshift(0);
+// console.log(a)
+// a.shift(0);
+// console.log(a)
+// a.reverse();
+// console.log(a)
+// console.log(a.indexOf(16))
+// console.log(a.includes(22))
+// var b=[15,24,8,13]
+// b.sort(function(a,b){return(a-b)});
+// console.log(b)
+// var c=["23","2053","27899","2345","24567"]
+// console.log(c.join(" "))
+
+
+
+
+
+//  questions with  multiple array methods 
+
+// var roses=["red","yellow","pink","white"]
+// console.log(roses)
+// roses.push("black")
+// console.log(roses)
+// roses.shift("red")
+// console.log(roses)
+
+
+// var array=[1,2,3,4,5,6,7,8]
+// array.reverse()
+// console.log(array)
+// console.log(array.join(" "))
+
+
+
+// var code=[1,9,8,7,6,5,4,3,2,1]
+// code.sort(function(a,b){return(a-b)});
+// console.log(code)
+// code.pop(
+// console.log(code)
+
+
+// var a =[10,15]
+// a.unshift(11,12)
+// console.log(a)
+// a.shift()
+// console.log(a)
+// console.log(a.length)
+
+var a=[100,99,98,97,96]
+var b=[95,94,93,92,91]
+var c=a.concat(b)
+console.log(c)
+c.sort(function(a,b){return(a-b)});
+console.log(c)
+c.pop()
+console.log(c)
